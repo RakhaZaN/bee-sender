9ffc09e3882060c21a470a1b324ea8a43387efd5
rakhazan <rakhazahnur26@gmail.com>
2022-04-22 04:48:30 +0700

    :tada: Push Project

---
+++ tailwind.config.js
@@ -0,0 +1,22 @@
+module.exports = {
+  content: ["./index.html"],
+  theme: {
+    fontFamily: {
+      inter: ['Inter, sans-serif'],
+      poppins: ['Poppins, sans-serif'],
+    },
+    container: {
+      center: true,
+      padding: {
+        DEFAULT: '1rem',
+      },
+      screens: {
+        lg: '1124px',
+        xl: '1124px',
+        '2xl': '1124px',
+      },
+    },
+    extend: {},
+  },
+  plugins: [],
+}
